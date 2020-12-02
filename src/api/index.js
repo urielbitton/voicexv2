feb0eb55ba92d9e287b6a550e86281e1da23865a
FJKhan <fjgenus@gmail.com>
2020-12-01 22:15:39 -0500

    updates gitignore to include node_modules. updates api/index.js and comps/Register.js to process registering a new user

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,14 @@
-
-export const signUp = userData => {
-    fetch(`http://localhost:3005/v1/users/sign-up`, userData). 
-    then( response => {console.log(response)}).catch(error => console.log(error));
-}
- 
+export const signUp = async (userData) => {
+  try {
+    const response = await fetch(`http://localhost:3005/v1/users/sign-up`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(userData)
+    });
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/comps/Register.js
+++ src/comps/Register.js
@@ -3,16 +3,17 @@ import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom"
 import {Inputs} from './Inputs'
 import {signUp} from '../api/index'
 
-function Register(props) {
+const Register = (props) => {
 
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [company, setCompany] = useState('')
   const [password, setPassword] = useState('')
 
-  function handleForm() {
+const handleForm = async () => {
     let userData = {email:email, password:password}
-    signUp(userData)
+    const result = await signUp(userData)
+    console.log(result)
   }
 
   return (
@@ -23,10 +24,10 @@ function Register(props) {
         <i class="fas fa-user-plus mainlogicon"></i>
         <h3>Create Account</h3> 
         <form> 
-          <Inputs title="Full Name" placeholder="Your full Name" onchange={() => setName()}/>
-          <Inputs title="Email" placeholder="Your email" onchange={() => setEmail()}/>
-          <Inputs title="Company Name" placeholder="Your company name" onchange={() => setCompany()}/>
-          <Inputs title="Password" type="password" placeholder="Enter a Password" onchange={() => setPassword()}/>
+          <Inputs title="Full Name" placeholder="Your full Name" onchange={e => setName(e)} />
+          <Inputs title="Email" placeholder="Your email" onchange={e => setEmail(e)} />
+          <Inputs title="Company Name" placeholder="Your company name" onchange={e => setCompany(e)} />
+          <Inputs title="Password" type="password" placeholder="Enter a Password" onchange={e => setPassword(e)} />
         </form>
         <button onClick={() => handleForm()}>Create Account</button>
         <small>Already have an account? <Link to="/login">Login</Link></small>
